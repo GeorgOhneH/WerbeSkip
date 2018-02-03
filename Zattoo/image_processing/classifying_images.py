cb185e4c7cd952799121be121d7a3102fa21d00b
GeorgOhneH <georg.schwan@edubs.ch>
2018-02-03 18:48:25 +0100

    added some positioning information of the images

--- Zattoo/image_processing/classifying_images.py
+++ Zattoo/image_processing/classifying_images.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     path_to_images = "prosieben/images/unclassified"
     paths_to_classes = [
         "prosieben/images/classified/logo_boarder_left_right",
-        "prosieben/images/classified/logo_boarder_up_down",
+        "prosieben/images/classified/logo_boarder_above_below",
         "prosieben/images/classified/logo",
         "prosieben/images/classified/no_logo",
         "prosieben/images/classified/special",
---
+++ Zattoo/image_processing/cropping images.py
@@ -0,0 +1,11 @@
+# Rations:
+# No Boarder: 16:9
+# Boarder left right: 8.5:11
+# Boarder above below: 2.25:1
+
+# coordinates of the position of the middle of the logo
+prosieben = {
+    "no_boarder": (922, 49),
+    "boarder_above_below": (922, 87),
+    "boarder_left_right": (807, 49)
+}
