b343c6bd2715565d6c5a6bdebafd8143c1a0312d
GeorgOhneH <georg.schwan@edubs.ch>
2018-02-12 11:26:32 +0100

    added nesterov to  SGDMomentum

--- Machine_Learning/optimizers/sgd_momentum.py
+++ Machine_Learning/optimizers/sgd_momentum.py
@@ -2,13 +2,17 @@ from .optimizer import Optimizer
 
 
 class SGDMomentum(Optimizer):
-    def __init__(self, learning_rate, momentum=0.9):
+    def __init__(self, learning_rate, momentum=0.9, nesterov=False):
         super().__init__(learning_rate)
+        self.nesterov = nesterov
         self.momentum = momentum
         self.velocity_w = 0
         self.velocity_b = 0
 
     def calculate_change(self, nabla_w, nabla_b):
+        if self.nesterov:
+            nabla_w += self.momentum * self.velocity_w
+            nabla_b += self.momentum * self.velocity_b
         self.velocity_w = self.momentum * self.velocity_w + self.learning_rate * nabla_w
         self.velocity_b = self.momentum * self.velocity_b + self.learning_rate * nabla_b
         return self.velocity_w, self.velocity_b
