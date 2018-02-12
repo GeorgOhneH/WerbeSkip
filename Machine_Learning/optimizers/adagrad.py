496767e84f93addfff25f585a06c488cdb8dc9db
GeorgOhneH <georg.schwan@edubs.ch>
2018-02-12 18:37:19 +0100

    changed adagrad

--- Machine_Learning/optimizers/adagrad.py
+++ Machine_Learning/optimizers/adagrad.py
@@ -7,12 +7,16 @@ class AdaGrad(Optimizer):
     def __init__(self, learning_rate, e=1e-8):
         super().__init__(learning_rate)
         self.e = e
-        self.cache_w = 0
-        self.cache_b = 0
+        self.caches = None
 
-    def calculate_change(self, nabla_w, nabla_b):
-        self.cache_w += np.power(nabla_w, 2)
-        self.cache_b += np.power(nabla_b, 2)
-        change_w = np.multiply(self.learning_rate / (np.sqrt(self.cache_w) + self.e), nabla_w)
-        change_b = np.multiply(self.learning_rate / (np.sqrt(self.cache_b) + self.e), nabla_b)
-        return change_w, change_b
+    def calculate_change(self, *nablas):
+        changes = []
+
+        if self.caches is None:
+            self.caches = [0 for _ in nablas]
+
+        for index, nabla in enumerate(nablas):
+            self.caches[index] += np.power(nabla, 2)
+            changes.append(np.multiply(self.learning_rate / (np.sqrt(self.caches[index]) + self.e), nabla))
+
+        return changes
