aa466be828f5ecebf2a55f627b396e35415fe353
GeorgOhneH <georg.schwan@edubs.ch>
2018-06-15 22:32:01 +0200

    generator can now be used

--- Thoughts.md
+++ Thoughts.md
@@ -4,4 +4,16 @@
 Die Generator Implantierung ist nicht so allgeimein Gültig und man müsste
 dann immer viel selber machen. Werde eine Hilfsklasse machen.
 
-Hab die fit funktion im Netzwerk aufgeräumt.
+Hab die fit funktion im Netzwerk aufgeräumt
+
+Generator classe sollte multithreadning können,
+da ich viel Zeit verliere durch http request und
+sonst ist es auch nicht schlecht. Probiere es mit einem
+[Condition Objects](https://docs.python.org/3/library/threading.html#condition-objects)
+und einer Queue.
+
+Implementiert. Probleme: Die Queue liste kann zu gross werden
+für den Speicher. Der Generator wird noch nicht
+beendet obwohl er schon fertig ist und landed in einem
+Deadlock
+
--- deepnet/utils/generator.py
+++ deepnet/utils/generator.py
@@ -1,10 +1,45 @@
+from queue import Queue
+from threading import Condition, Thread
+
+
 class Generator(object):
-    def __init__(self, epochs, mini_batch_size):
+    def __init__(self, epochs, mini_batch_size, n_workers):
+        self.n_workers = n_workers
         self.mini_batch_size = mini_batch_size
         self.epochs = epochs
+        self.threads = []
+        self.queue = Queue()
+        self.cv = Condition()
+        self.init()
+
+    def init(self):
+        indexes = [range(x, self.__len__(), self.n_workers) for x in range(self.n_workers)]
+        self.threads = [Thread(target=self.produce_item, args=(index,)) for index in indexes]
+        for thread in self.threads:
+            thread.start()
+
+    def __iter__(self):
+        return self
 
     def __next__(self):
-        raise NotImplemented
+        print(self.queue.qsize())
+        with self.cv:
+            while self.queue.empty():
+                self.cv.wait()
+            mini_batch = self.queue.get()
+            self.queue.task_done()
+            return mini_batch
 
     def __len__(self):
-        return 1
+        raise NotImplemented
+
+    def produce_item(self, indexes):
+        for index in indexes:
+            with self.cv:
+                mini_batches = self.get_mini_batches(index)
+                for mini_batch in mini_batches:
+                    self.queue.put(mini_batch)
+
+    def get_mini_batches(self, index) -> list:
+        pass
+
