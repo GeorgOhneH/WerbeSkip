3461f5645a701ae85c52ed4d0a2676f833e4b71b
GeorgOhneH <georg.schwan@edubs.ch>
2018-09-03 17:17:49 +0200

    fix?

--- vuedj/asgi.py
+++ vuedj/asgi.py
@@ -7,6 +7,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multichat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vuedj.settings")
 django.setup()
 application = get_default_application()
