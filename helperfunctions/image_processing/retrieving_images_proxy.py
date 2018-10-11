cd6d3e2778ed7ee8fc9c08aac6daf5b93669511a
GeorgOhneH <georg.schwan@edubs.ch>
2018-10-11 16:16:02 +0200

    updated VideoCapture

--- helperfunctions/image_processing/retrieving_images_proxy.py
+++ helperfunctions/image_processing/retrieving_images_proxy.py
@@ -9,12 +9,12 @@ from threading import Thread
 
 
 class VideoCapture(object):
-    def __init__(self, channel: int, rate_limit=None, convert_network=False):
+    def __init__(self, channel: int, rate_limit=30, convert_network=False):
         self.pipe = None
         self.m3u8_update_thread = None
         self.last_m3u8 = None
         self.convert_network = convert_network
-        self.rate_limit = rate_limit  # images per second
+        self.rate_limit = rate_limit  # frames per second
         self.session = requests.session()
         self.setup_cookies()
         self.cap_url = self.get_cap_url(channel)
@@ -24,6 +24,7 @@ class VideoCapture(object):
         self.PATH_TO_TS_FILES = os.path.dirname(os.path.dirname(__file__))  # ffmpeg is a bit weird with the paths
         self.M3U8_NAME = "index.m3u8"
         self.proxies = {}
+        self.ts_files = []
         self.init()
 
     def setup_cookies(self):
@@ -55,6 +56,10 @@ class VideoCapture(object):
         return cap_url
 
     def init(self):
+        for file in os.listdir(self.PATH_TO_TS_FILES):
+            if ".ts" in file:
+                os.remove(os.path.join(self.PATH_TO_TS_FILES, file))
+
         self.update_m3u8_file()
 
         cmd_out = ['ffmpeg',
@@ -64,6 +69,7 @@ class VideoCapture(object):
                    '-vcodec', 'rawvideo',
                    '-c:v', 'rawvideo',
                    '-f', 'image2pipe',
+                   '-r', str(self.rate_limit),  # framerate
                    '-']
 
         self.pipe = sp.Popen(cmd_out, bufsize=10 ** 8, stdout=sp.PIPE)
@@ -74,14 +80,20 @@ class VideoCapture(object):
     def update_m3u8_file(self):
         text = requests.get(self.cap_url).text
         if text != self.last_m3u8:
+            if len(self.ts_files) > 5:
+                for file_name in self.ts_files[:-4]:
+                    os.remove(file_name)
+                    self.ts_files.remove(file_name)
             self.last_m3u8 = text
             with open(os.path.join(self.PATH_TO_CACHE, self.M3U8_NAME), mode="w") as f:
                 f.write(text)
             for file_name in text.splitlines():
-                if ".ts" in file_name and not os.path.exists(os.path.join(self.PATH_TO_TS_FILES, file_name)):
+                path_to_ts_file = os.path.join(self.PATH_TO_TS_FILES, file_name)
+                if ".ts" in file_name and not os.path.exists(path_to_ts_file):
+                    self.ts_files.append(path_to_ts_file)
                     file_url = self.cap_url[:-10] + file_name
                     data = requests.get(file_url).content
-                    with open(os.path.join(self.PATH_TO_TS_FILES, file_name), mode="wb") as f:
+                    with open(path_to_ts_file, mode="wb") as f:
                         f.write(data)
 
     def m3u8_thread(self):
